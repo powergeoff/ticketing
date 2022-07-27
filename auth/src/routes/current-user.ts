75142f10e695c45a4a0512ba14e38e8bcde8d453
Geoff O'Connor <oconnor.geoffrey@gmail.com>
2022-07-27 13:56:33 -0400

    Section 9: Authentication Strategies and Options

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,23 +1,14 @@
 //application cannot discern whether the jwt is valid or not
 //need a service call
 import express, {Request, Response} from 'express';
-import { body } from 'express-validator';
-import jwt from 'jsonwebtoken';
-
-
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
-  }
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.send({ currentUser: payload });
-  } catch(err){
-    res.send({ currentUser: null });
-  }
+router.get('/api/users/currentuser', currentUser,
+(req: Request, res: Response) => {
+  
+  res.send({ currentUser: req.currentUser || null });
   
 });
 

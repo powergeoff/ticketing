e5de8d5e8783805c57b323c26cfd9220d5687d6d
Geoff O'Connor <oconnor.geoffrey@gmail.com>
2022-08-03 11:44:28 -0400

    End of Sectiion 11 - bring auth into client app

---
+++ client/hooks/use-request.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { useState } from 'react';
+
+const useRequest = ({ url, method, body, onSuccess }) => {
+  //method = post, put, get etcc...
+  const [errors, setErrors] = useState(null);
+
+  const doRequest = async () => {
+    try {
+      setErrors(null);
+      const response = await axios[method](url, body);
+
+      if (onSuccess) {
+        onSuccess(response.data);
+      }
+      return response.data;
+    } catch (err) {
+      setErrors(
+        <div className='alert alert-danger'>
+          <h4>Ooops...</h4>
+          <ul className='my-0'>
+            {err.response.data.errors.map(e => 
+              <li key={e.message}>{e.message}</li>
+            )}
+          </ul>
+        </div>
+      )
+    }
+  };
+
+  return { doRequest, errors }
+}
+
+export default useRequest;
--- client/pages/banana.js
+++
@@ -1,5 +0,0 @@
-const Banana = () => {
-  return <h1>Banana You</h1>;
-}
-
-export default Banana;

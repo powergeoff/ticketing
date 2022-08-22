4107fa6933aaf69b6d31bc6e5188a01f4d1b6517
Geoffrey A. O'Connor <oconnor.geoffrey@gmail.com>
2022-08-22 08:10:04 -0400

    remove common folder from repo

---
+++ tickets/src/events/publishers/ticket-created-publisher.ts
@@ -0,0 +1,6 @@
+import { Publisher, Subjects, TicketCreatedEvent } from '@pwrgtickets/common';
+
+export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+}
+
